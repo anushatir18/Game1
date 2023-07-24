848b97daeaf5ab44ce36aeaa572aad8ba7e39cfe
anushatir18 <140386765+anushatir18@users.noreply.github.com>
2023-07-24 22:34:38 +0530

    almost

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,12 +1,13 @@
 var stone ="img/stone.png";
 var scissor ="img/scissor.png";
 var paper ="img/paper.png";
-   
-function number(){
-    const x = Math.random();
-    const scaledNumber = x * 3;
-    const finalNumber = Math.floor(scaledNumber) + 1;
 
+var x = Math.random();
+var scaledNumber = x * 3;
+var finalNumber = Math.floor(scaledNumber) + 1;
+var user;
+
+function number(){
     const imageElement = document.getElementById('img');
 
     if(finalNumber==1){
@@ -19,18 +20,64 @@ function number(){
         imageElement.src = scissor;
     }
 
+    result();
+
 } 
 
-function stone(){
+function result(){
+if(finalNumber==user ){
+    document.getElementById('result').innerHTML = "SAME";
+}
+else if(finalNumber==1 && user==2 ){
+    document.getElementById('result').innerHTML = "WIN";
+}
+else if(finalNumber==1 && user==3 ){
+    document.getElementById('result').innerHTML = "LOOSE";
+}
+else if(finalNumber==2 && user==1 ){
+    document.getElementById('result').innerHTML = "LOOSE";
+}
+else if(finalNumber==2 && user==3 ){
+    document.getElementById('result').innerHTML = "WIN";
+}
+else if(finalNumber==3 && user==1 ){
+    document.getElementById('result').innerHTML = "WIN";
+}
+else if(finalNumber==3 && user==2 ){
+    document.getElementById('result').innerHTML = "LOOSE";
+}
+}
+
+function updateOutput() {
+  document.getElementById('test').innerText = user;
+  
+}
+
+function stonee() {
+    user = 1;
     const imageElement = document.getElementById('imgg');
+    document.getElementById('test').innerHTML= user;
     imageElement.src = stone;
+    updateOutput();
+    
 }
-function paper(){
+
+function paperr() {
+    user = 2;
     const imageElement = document.getElementById('imgg');
+    document.getElementById('test').innerHTML= user;
     imageElement.src = paper;
+    updateOutput();
+    
 }
-function scissor(){
+
+function scissorr() {
+    user = 3;
     const imageElement = document.getElementById('imgg');
-    imageElement.src = scissor;
+    document.getElementById('test').innerHTML= user;
+    imageElement.src =scissor;
+    updateOutput();
+    
 }
+
 
